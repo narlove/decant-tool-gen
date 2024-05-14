8b500a043e0d4b0dfa0b82d9aadaaa38a0d192c9
Nathan <natey@outlook.com.au>
2024-05-14 17:12:15 +1000

    added data structures, reading and parsing keycodes

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,51 @@
 using namespace csv;
 using std::string;
 
+const int MAX_BARCODE_NUMBER = 10;
+
+template <typename T>
+struct barcode_array_wrapper
+{
+    T data[MAX_BARCODE_NUMBER];
+    unsigned int size;
+
+    barcode_array_wrapper(T default_val)
+    {
+        this->size = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = default_val;
+        }
+    }
+
+    void add(T val)
+    {
+        data[size] = val;
+        size++;
+    }
+};
+
 int main()
 {
+    barcode_array_wrapper<unsigned int> array(0);
+
     CSVFormat format;
-    format.delimiter(',').no_header();
+    format.delimiter(',').no_header(); // so that the file gets data from all rows
 
-    CSVReader reader("../target/test-file.csv", format);
+    CSVReader barcodes("../target/scanned-barcodes.csv", format);
 
-    for (CSVRow &row : reader)
+    for (CSVRow &row : barcodes)
     {
         for (CSVField &field : row)
         {
-            printf("%s, ", field.get<string>().c_str());
+            array.add(field.get<unsigned int>());
         }
+    }
 
-        printf("\n");
+    for (int i = 0; i < array.size; i++)
+    {
+        printf("%u\n", array.data[i]);
     }
 
     return 0;
