80275637ee23481af2cd2bfdcc7772f25a1dadb8
Nathan <natey@outlook.com.au>
2024-05-18 10:42:31 +1000

    updated organisation, changed names for readability, added 2 calc functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,78 +3,113 @@
 #include "csv.hpp"
 #include <unordered_map>
 #include <stdexcept>
+#include <vector>
+#include <stdlib.h>
 
 using std::invalid_argument;
+using std::malloc;
 using std::unordered_map;
+using std::vector;
 using namespace csv;
 using std::string;
 
-const int MAX_BARCODE_NUMBER = 10;
+const int MAX_PALLET_NUMBER = 10;
 
-const int PALLETS_ASSOCIATED_DATA_NUMBER = 2;
+// was really long and didn't wanna type it all the time.
+// almost over engineered this, but i think i steered it back down to be readable enough.
+typedef unordered_map<unsigned int, unsigned int> keycode_size_type;
 
-// can i make this use inheritance to simplify, instead of two of the example same struct?
-template <typename T>
-struct keycode_array_wrapper
+struct keycodes_wrapper
 {
-    T data[MAX_BARCODE_NUMBER];
+    unsigned int data[MAX_PALLET_NUMBER];
     unsigned int size;
-
-    keycode_array_wrapper(T default_val)
-    {
-        this->size = 0;
-
-        for (int i = 0; i < size; i++)
-        {
-            data[i] = default_val;
-        }
-    }
-
-    void add(T val)
-    {
-        data[size] = val;
-        size++;
-    }
 };
 
-// TODO: tests?
-int main()
+struct carton_sizes_wrapper
 {
-    keycode_array_wrapper<unsigned int> keycodes(0);
-    unordered_map<unsigned int, unsigned int> keycode_cartons;
+    unsigned int data[MAX_PALLET_NUMBER];
+    unsigned int size;
+};
 
-    CSVFormat format;
-    format.delimiter(',').no_header(); // so that the file gets data from all rows
+// array wrapper function(s)
+// just incase i have multiple array wrappers
+template <typename T>
+void array_add(T array[], unsigned int &size, T val)
+{
+    array[size] = val;
+    size++;
+}
 
-    // load keycodes
+void read_pallet_information(keycode_size_type &keycode_size_map, CSVFormat format)
+{
+    CSVReader pallet_info("../target/pallets.csv", format);
+    for (CSVRow &row : pallet_info)
+    {
+        // 2 = 1 (for the keycode) + 1 (for the size)
+        if (row.size() != 2)
+        {
+            throw invalid_argument("invalid arguments provided to the pallets.csv file");
+        }
+
+        keycode_size_map[row[0].get<unsigned int>()] = row[1].get<unsigned int>();
+    }
+}
+
+void read_keycodes(keycodes_wrapper &keycodes, CSVFormat format)
+{
     CSVReader read_keycodes("../target/keycodes.csv", format);
     for (CSVRow &row : read_keycodes)
     {
         for (CSVField &field : row)
         {
-            keycodes.add(field.get<unsigned int>());
+            array_add<unsigned int>(keycodes.data, keycodes.size, field.get<unsigned int>());
         }
     }
+}
 
-    // maybe reading from a csv and returning into a reference to an array is a function?
-    // loading the pallets file is basically the SQL database here... actually, maybe i learn sql for the hd project?!
-    CSVReader read_pallet_information("../target/pallets.csv", format);
-    for (CSVRow &row : read_pallet_information)
+// procedure so that we're not returning a big data type
+// using arrays because showing gained knowledge, otherwise would use a vector
+
+// fix so that it uses a new array wrapper
+void extract_carton_count(keycode_size_type &keycode_size_map, carton_sizes_wrapper carton_sizes)
+{
+    for (auto keycode_size : keycode_size_map)
     {
-        if (row.size() != PALLETS_ASSOCIATED_DATA_NUMBER)
-        {
-            throw invalid_argument("invalid arguments provided to the pallets.csv file");
-        }
+        // adding to the sizes array, the size that we find for each keycode.
+        array_add<unsigned int>(carton_sizes.data, carton_sizes.size, keycode_size.second);
+    }
+}
 
-        keycode_cartons[row[0].get<unsigned int>()] = row[1].get<unsigned int>();
+// fix so that it uses a new array wrapper
+unsigned int calculate_total_cartons(carton_sizes_wrapper sizes)
+{
+    unsigned int sum = 0;
+    for (unsigned int size : sizes.data)
+    {
+        sum += size;
     }
 
-    // printing the array
-    // a function, maybe a CALLBACK?!!?
-    // doing it this way because i think thats its easier to read than using "auto" and .first, .second, etc.
+    return sum;
+};
+
+// TODO: tests?
+int main()
+{
+    keycodes_wrapper keycodes = {.size = 0};
+    keycode_size_type keycode_size_map;
+
+    CSVFormat format;
+    format.delimiter(',').no_header(); // so that the file gets data from all rows
+
+    // was going to remove keycodes, but the reason its here is for easy read and write if
+    // i get a barcode scanner or similar
+    read_keycodes(keycodes, format);
+
+    read_pallet_information(keycode_size_map, format);
+
     for (int i = 0; i < keycodes.size; i++)
     {
-        printf("Pallet with keycode %u has %u cartons on it\n", keycodes.data[i], keycode_cartons[keycodes.data[i]]);
+        printf("Imported pallet %u has data (from lookup): CARTON_SIZE: %u;\n", keycodes.data[i], keycode_size_map[keycodes.data[i]]);
     }
 
     return 0;
